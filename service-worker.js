2601f9a3a9a7c29eb223578cbb45c6aea894b1a6
pongowarrior <nathangregoire33@gmail.com>
2025-09-29 12:19:19 +0100

    Fairshare full upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("fairshare-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json",
+        "icons/icon-192.png",
+        "icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
